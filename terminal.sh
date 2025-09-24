0bfe3fa9cd24aecd858829399451a1f7bf543198
beta7developer <viethoang082013chess@outlook.com.vn>
2025-09-24 16:56:43 +0700

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -7,9 +7,9 @@ read -p "$ ~ >> " input
 
 if [[ "$input" == "pacman -S conandpkg.zip" ]]; then
     wget "https://github.com/beta7developer/src-conand/archive/refs/heads/bin.zip"
-    wget "https://github.com/beta7developer/src-conand/archive/refs/heads/src.zip"
-    wget "https://github.com/beta7developer/src-conand/archive/refs/heads/scrapped.zip"
     wget "https://github.com/beta7developer/src-conand/archive/refs/heads/retro.zip"
+    wget "https://github.com/beta7developer/src-conand/archive/refs/heads/scrapped.zip"
+    wget "https://github.com/beta7developer/src-conand/archive/refs/heads/src.zip"
 else
     echo "bash: invalid command"
 fi
