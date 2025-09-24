82646d6d9dab606dbbe54216ca726bb014f3e9d7
beta7developer <viethoang082013chess@outlook.com.vn>
2025-09-24 16:43:37 +0700

    Add files via upload

---
+++ terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Welcome to Console AndroidOS Terminal"
+echo "Type help for help."
+
+read -p "$ ~ >> " input
+
+if [[ "$input" == "pacman -S conandpkg.zip" ]]; then
+    wget "https://github.com/beta7developer/Console-AndroidOS-Simulation/archive/refs/heads/main.zip"
+else
+    echo "bash: invalid command"
+fi
